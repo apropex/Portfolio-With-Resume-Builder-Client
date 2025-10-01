111d3c299f96550b3f87d36cc0bbf2237193c54f
Muhammad Abdullah <pxga.abdullah@hotmail.com>
2025-10-01 14:08:49 +0600

    updated particle background

--- src/components/layouts/navbar/NavLinks.tsx
+++ src/components/layouts/navbar/NavLinks.tsx
@@ -8,32 +8,7 @@ import { navigationLinks } from "./Navbar";
 export default function NavLinks() {
   const pathName = usePathname();
 
-  console.log({ pathName });
-
   return (
-    /*
-    <NavigationMenu>
-      <NavigationMenuList className="gap-2">
-        {navigationLinks?.map((link, index) => (
-          <NavigationMenuItem key={index}>
-            <NavigationMenuLink
-              active={link.active}
-              href={link.href}
-              className="text-muted-foreground hover:text-primary py-1.5 font-medium px-3.5"
-            >
-              {link.label}
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
-
-
-    
-
-
-*/
-
     <div>
       {navigationLinks?.map((link, index) => (
         <Link href={link.href} key={index}>
@@ -43,7 +18,7 @@ export default function NavLinks() {
               {
                 "focus:bg-accent hover:bg-accent border border-green-500/10 bg-accent text-accent-foreground":
                   pathName === link.href,
-              },
+              }
             )}
           >
             {link.label}
