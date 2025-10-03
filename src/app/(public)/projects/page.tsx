67f7930fe4babdd346b0e9c3e04cf67f6c62faae
Muhammad Abdullah <pxga.abdullah@hotmail.com>
2025-10-03 18:33:01 +0600

    added rich text editor

--- src/app/(public)/projects/page.tsx
+++ src/app/(public)/projects/page.tsx
@@ -1,9 +1,12 @@
 //
 
+import RichTextEditor from "@/components/modules/Blog/RichTextEditor";
+
 export default function ProjectsPage() {
   return (
     <div className="">
       <h1 className="">This is ProjectsPage component</h1>
+      <RichTextEditor />
     </div>
   );
 }
